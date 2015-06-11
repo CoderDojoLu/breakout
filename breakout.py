2b9f89c54873361a5e7ecd1f2c962b2a5661c99f
Tim Golden <mail@timgolden.me.uk>
2015-06-11 08:57:12 +0100

    More simplifications

--- breakout.py
+++ breakout.py
@@ -1,81 +1,123 @@
 import time
 
-W = 800
-H = 600
-RED = 200, 0, 0
-BLUE = 0, 0, 200
-GREEN = 0, 200, 0
+WIDTH = 640
+HEIGHT = 480
 
-ball = Rect((W/2, H/2), (30, 30))
-ball_dir = 5, -5
+#
+# The ball is a square red block which starts in the middle
+# of the screen moving towards the lower-right.
+#
+ball = Rect(WIDTH / 2, HEIGHT / 2, 30, 30)
+ball.direction = 5, -5
+ball.colour = "red"
 
-bat = Rect(0, 0, 150, 15)
-bat.midbottom = screen.centrex, screen.bottom - 10
-#~ bat = Rect((W/2, 0.96 * H), (150, 15))
+#
+# The bat is a green oblong which starts just above the bottom
+# of the screen and halfway across.
+#
+bat = Rect(WIDTH / 2, 0.96 * HEIGHT, 150, 15)
+bat.colour = "green"
+
 N_BLOCKS = 8
-BLOCK_W = W / N_BLOCKS
+BLOCK_W = WIDTH / N_BLOCKS
 BLOCK_H = BLOCK_W / 4
-BLOCK_COLOURS = RED, GREEN, BLUE
+BLOCK_COLOURS = "purple", "lightgreen", "lightblue", "orange"
 
+#
+# Create <N_BLOCKS> blocks, filling the screen. Each block is as
+# high as a quarter of its width, making them proportional as the
+# number of blocks or the screen size changes.
+#
+# The block colours cycle through <BLOCK_COLOURS>
+#
 blocks = []
 for n_block in range(N_BLOCKS):
-    colour = BLOCK_COLOURS[n_block % len(BLOCK_COLOURS)]
     block = Rect(n_block * BLOCK_W, 0, BLOCK_W, BLOCK_H)
-    block.colour = colour
+    block.colour = BLOCK_COLOURS[n_block % len(BLOCK_COLOURS)]
     blocks.append(block)
 
 def draw_blocks():
+    """Draw all the remaining blocks on the screen, each in its
+    own colour and at its own position. Any blocks which have
+    been "killed" won't be in the list and so won't be drawn.
+    """
     for block in blocks:
         screen.draw.filled_rect(block, block.colour)
 
 def draw():
+    """Redraw the entire screen:
+    
+    * Clear the background
+    * Draw the ball
+    * Draw the bat
+    * Draw the blocks
+    """
     screen.clear()
-    screen.draw.filled_rect(ball, RED)
-    screen.draw.filled_rect(bat, RED)
+    screen.draw.filled_rect(ball, ball.colour)
+    screen.draw.filled_rect(bat, bat.colour)
     draw_blocks()
 
 def on_mouse_move(pos):
-    x, y = pos
-    bat.center = (x, bat.center[1])
-
-def on_mouse_down():
-    global ball_dir
-    x, y = ball_dir
-    ball_dir = x * 1.5, y * 1.5
-
-def move(ball):
-    """returns a new ball at a new position
+    """Make the bat follow the horizontal movement of the mouse.
+    Make sure it doesn't run off the left or right of the screen
     """
-    global ball_dir
-    dx, dy = ball_dir
+    x, y = pos
+    bat.centrex = x
+    bat.left = max(bat.left, 0)
+    bat.right = min(bat.right, WIDTH)
+
+def update():
+    """Move the ball and then determine what effect its new position has
+    """
+    #
+    # Move the ball along its current direction
+    #
+    dx, dy = ball.direction
     ball.move_ip(dx, dy)
+    
+    #
+    # Bounce the ball off the right or left walls
+    #
+    if ball.right >= WIDTH or ball.left <= 0:
+        sounds.blip.play()
+        ball.direction = -dx, dy
 
-    if ball.x > W or ball.x <= 0:
-        ball_dir = -dx, dy
+    #
+    # Bounce the ball off the top wall
+    #
+    if ball.top <= 0:
+        sounds.blip.play()
+        ball.direction = dx, -dy
 
-    if ball.y <= 0:
-        ball_dir = dx, -dy
-
+    #
+    # Bounce the ball off the bat
+    #
     if ball.colliderect(bat):
         sounds.blip.play()
-        ball_dir = dx, -abs(dy)
+        ball.direction = dx, -dy
 
+    #
+    # If the ball hits a rectangle, kill that rectangle and
+    # bounce the ball.
+    #
     to_kill = ball.collidelist(blocks)
     if to_kill >= 0:
         sounds.block.play()
-        ball_dir = dx, abs(dy)
         blocks.pop(to_kill)
+        ball.direction = dx, -dy
 
+    #
+    # If there are no blocks left, you win
+    #
     if not blocks:
-        sounds.win.play()
-        sounds.win.play()
+        sounds.win.play(3)
         time.sleep(1)
         exit()
 
-    if ball.y > H:
+    #
+    # If the ball hits the bottom of the screen, you lose
+    #
+    if ball.bottom >= HEIGHT:
         sounds.die.play()
         time.sleep(1)
         exit()
-
-def update():
-    move(ball)
